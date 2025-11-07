c8e8b109e5cce5a749a477299ff03f9070077bd6
anacg05 <anagc09@outlook.com>
2025-11-07 11:29:30 -0300

    07/11

---
+++ GerenciamentoVariaveis/src/Instrutor.java
@@ -0,0 +1,12 @@
+public class Instrutor {
+    String nome;
+
+    public Instrutor(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return this.nome;
+    }
+}
